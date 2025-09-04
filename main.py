c72061c2696cccbc3294928690413c84a6d5ecbb
CallsignNavy <callsignnavy.business@gmail.com>
2025-09-03 21:41:39 -0400

    defined user_prompt to hold the sys.argv[1] input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#python
 import os
 import sys
 from dotenv import load_dotenv
@@ -6,9 +7,9 @@ from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
 
+user_prompt = sys.argv[1]
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
@@ -17,17 +18,17 @@ def main():
     print("Hello from bootaiagent!")
 
     if len(sys.argv) == 2:
+
         resp = client.models.generate_content(
             model="gemini-2.0-flash-001",
-            contents = messages,
+            contents=messages,
         )
         print(resp.text)
+        print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
     else:
         print("Error: Enter a prompt")
         sys.exit(1)
-    print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
-
 
 if __name__ == "__main__":
     main()
