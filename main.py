c0811de60b4d5dd16537554ed220b2dca626c421
CallsignNavy <callsignnavy.business@gmail.com>
2025-09-04 22:10:40 -0400

    Used the recommended structure from Boots on how main.py should look

--- main.py
+++ main.py
@@ -12,29 +12,32 @@ client = genai.Client(api_key=api_key)
 def main():
     print("Hello from bootaiagent!")
 
-    if len(sys.argv) == 3:
-        user_prompt = sys.argv[1]
-        verbose = (len(sys.argv) == 3 and sys.argv[2] == "--verbose")
-        
-        #verbose check
-        if len(sys.argv) < 2 or len(sys.argv) > 3:
-            print("Error: Enter a prompt, optionally followed by --verbose")
-            sys.exit(1)
-        if len(sys.argv) == 3 and sys.argv[2] != "--verbose":
-            print("Error: Unknown option. Prompts must be followed with --verbose")
-            sys.exit(1)
+    
+    #verbose check
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("Error: Enter a prompt, optionally followed by --verbose")
+        sys.exit(1)
+    if len(sys.argv) == 3 and sys.argv[2] != "--verbose":
+        print("Error: Unknown option. Prompts must be followed with --verbose")
+        sys.exit(1)
 
+    user_prompt = sys.argv[1]
+    verbose = (len(sys.argv) == 3 and sys.argv[2] == "--verbose")
 
-        messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-        resp = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages,
-        )
+    if verbose:
+        print(f"User prompt: {user_prompt}")
 
-        if verbose:
-            print(f"User prompt: {resp.text}")
-            print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    resp = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    print(resp.text)
+
+    if verbose:
+        print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
